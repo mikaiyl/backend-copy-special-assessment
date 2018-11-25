11bf42e905a85cc9313c7fe846d03e4566bf59f8
= <mikaiyl.davis@icloud.com>
2018-11-25 14:16:53 -0500

    fixed flake8

--- copyspecial.py
+++ copyspecial.py
@@ -6,7 +6,6 @@
 # Google's Python Class
 # http://code.google.com/edu/languages/google-python-class/
 
-import sys
 import re
 import os
 import shutil
@@ -18,51 +17,66 @@ from zipfile import ZipFile
 # change this for a
 SPECIAL_RE = re.compile(r'.+__\w+__.+')
 
-def get_special( dirname ):
-    #  returns a list of the absolute paths of the special files in the given directory
+
+def get_special(dirname):
+    #  returns a list of the absolute paths of
+    #  the special files in the given directory
     global SPECIAL_RE
-    sp_list = filter( lambda f: bool( SPECIAL_RE.match( f ) ), os.listdir( dirname ) )
-    return map( lambda f: os.path.join( os.getcwd(), f ), sp_list )
+    sp_list = filter(lambda f: bool(SPECIAL_RE.match(f)), os.listdir(dirname))
+    return map(lambda f: os.path.join(os.getcwd(), f), sp_list)
 
 
-def copy_to( files, dest ):
-    # given a list of paths, copies those files into the given directory
+def copy_to(files, dest):
+    # given a list of paths,
+    # copies those files into the given directory
     for file in files:
-        shutil.copyfile( file, os.path.join( dest, file.split('/')[-1] ) )
+        shutil.copyfile(file, os.path.join(dest, file.split('/')[-1]))
     return
 
 
-def zip( files, dest ):
-    # given a list of paths, zip those files up into the given zipfile
-    with ZipFile( dest, 'w' ) as archive:
+def zip(files, dest):
+    # given a list of paths, zip
+    # those files up into the given zipfile
+    with ZipFile(dest, 'w') as archive:
         for file in files:
-            archive.write( file )
+            archive.write(file)
 
 
-def zip_to( files, dest ):
-    """If the "--tozip zipfile" option is present at the start of the command
-    line, run this command: "zip -j zipfile <list all the files>". This will
-    create a zipfile containing the files. Just for fun/reassurance, also print
-    the command line you are going to do first (as shown in lecture). (Windows
-    note: windows does not come with a program to produce standard .zip
-    archives by default, but you can get download the free and open zip
-    program from [www.info-zip.org](http://www.info-zip.org/).)"""
-    command = 'zip -j {zipfile} {filez}'.format( zipfile=dest, filez=' '.join( files ) )
+def zip_to(files, dest):
+    """
+    If the "--tozip zipfile" option is
+    present at the start of the command
+    line, run this command:
+    "zip -j zipfile <list all the files>".
+    This will create a zipfile containing
+    the files. Just for fun/reassurance,
+    also print the command line you are
+    going to do first (as shown in lecture).
+    (Windows note: windows does not come
+    with a program to produce standard .zip
+    archives by default, but you can get
+    download the free and open zip program
+    from [www.info-zip.org](http://www.info-zip.org/).)
+    """
+
+    command = 'zip -j {z} {f}'.format(z=dest, f=' '.join(files))
     # this became a tuple IDK why?
     # print( 'Command I\'m going to try:' + '\n' + command )
-    print 'Command I\'m going to try:' + '\n' + command
-    commands.getoutput( command )
+    print_str = 'Command I\'m going to try:' + '\n' + command
+    print(print_str)
+    commands.getoutput(command)
     return
 
 # +++your code here+++
 # Write functions and modify main() to call them
+
 
 def main():
     # This snippet will help you get started with the argparse module.
     parser = argparse.ArgumentParser()
     parser.add_argument('--todir', help='dest dir for special files')
     parser.add_argument('--tozip', help='dest zipfile for special files')
-    parser.add_argument('dirname', type=str, help='dest zipfile for special files')
+    parser.add_argument('dirname', type=str, help='dest for special files')
     # TODO need an argument to pick up 'from_dir'
     args = parser.parse_args()
 
@@ -70,28 +84,29 @@ def main():
     # Read the docs and examples for the argparse module about how to do this.
 
     # Parsing command line arguments is a must-have skill.
-    # This is input data validation.  If something is wrong (or missing) with any
-    # required args, the general rule is to print a usage message and exit(1).
+    # This is input data validation.  If something is wrong (or missing) with
+    # any required args, the general
+    # rule is to print a usage message and exit(1).
 
     # +++your code here+++
     # Call your functions
     #
 
     if args.todir:
-        if not os.path.exists( args.todir ):
-            os.mkdir( os.path.join( os.getcwd(), args.todir ) )
+        if not os.path.exists(args.todir):
+            os.mkdir(os.path.join(os.getcwd(), args.todir))
         else:
             pass
-        copy_to( get_special( args.dirname ), args.todir )
+        copy_to(get_special(args.dirname), args.todir)
     elif args.tozip:
-        if os.path.exists( args.tozip ):
-            print( 'Filename already exists' )
+        if os.path.exists(args.tozip):
+            print('Filename already exists')
             return 1
         else:
-            zip_to( get_special( args.dirname ), args.tozip )
+            zip_to(get_special(args.dirname), args.tozip)
     else:
-        for file in get_special( args.dirname ):
-            print( file )
+        for file in get_special(args.dirname):
+            print(file)
 
 
 if __name__ == "__main__":
